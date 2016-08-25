f36cd2a27983c214b53c25d8743dc69695d3a986
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-25 17:39:40 -0400

    Update LC-026.java

--- Array/LC-026.java
+++ Array/LC-026.java
@@ -1,4 +1,8 @@
 public class Solution {
+    
+    /**
+     * Remove Duplicates;
+     */
     public int removeDuplicates(int[] nums) {
         if (nums.length < 1)
             return 0;
@@ -16,5 +20,36 @@ public class Solution {
         }
         
         return slow + 1;
+    }
+    
+    /**
+     * Duplicates are allowed at most twice;
+     */
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 1)
+            return 0;
+        if (nums.length == 1)
+            return 1;
+            
+        int start, idx;
+        if (nums[0] == nums[1]) {
+            start = 2;
+            idx = 1;
+        } else {
+            start = 1;
+            idx = 0;
+        }
+    
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == nums[idx])
+                continue;
+            else {
+                nums[++idx] = nums[i];
+                if (i + 1 < nums.length && nums[i] == nums[i + 1])
+                    nums[++idx] = nums[++i];
+            }
+        }
+        
+        return idx + 1;
     }
 }

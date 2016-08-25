d5ed029c3ee50b9b0b31fbd85f3b01c72ca1e5be
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-25 19:47:44 -0400

    Update LC-075.java

--- Array/LC-075.java
+++ Array/LC-075.java
@@ -1,6 +1,6 @@
 public class Solution {
     
-    //Count Sort
+    //Counting Sort
     public void sortColors(int[] nums) {
         int n1 = 0, n2 = 0, n3 = 0;
         
@@ -19,6 +19,26 @@ public class Solution {
         for(int i = n1 + n2; i < n1 + n2 + n3; i++)
             nums[i] = 2;
             
+        return;
+    }
+    
+    //One Pass, Two Pointers
+    public void sortColors(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        
+        for(int i = 0; i <= right; i++) {
+            while(nums[i] == 2 && i < right) {
+                int tmp = nums[i];
+                nums[i] = nums[right];
+                nums[right--] = tmp;
+            }
+            while(nums[i] == 0 && i > left) {
+                int tmp = nums[i];
+                nums[i] = nums[left];
+                nums[left++] = tmp;
+            }
+        }
+        
         return;
     }
 }

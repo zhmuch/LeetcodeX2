7dcc14e2d153beb5a2a19742900f61ee9dbf08ab
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-25 01:11:45 -0400

    Update LC-RotatedSortedArray.java

--- Array/LC-RotatedSortedArray.java
+++ Array/LC-RotatedSortedArray.java
@@ -27,4 +27,37 @@ public class Solution {
         }
         return -1;
     }
+    
+    /**
+     * With Duplicates
+     */
+    public boolean search(int[] nums, int target) {
+        int len = nums.length;
+        if (len < 1)
+            return false;
+        
+        int left = 0, right = len - 1;
+        while(left <= right) {
+            int med = (left + right) / 2;
+            if (nums[med] == target)
+                return true;
+             else if (nums[med] > nums[left]) {
+                if (nums[med] > target && target >= nums[left])
+                    right = med - 1;
+                else
+                    left = med + 1;
+            } else if (nums[med] < nums[left]) {
+                if (nums[med] < target && target <= nums[right])
+                    left = med + 1;
+                else
+                    right = med - 1;
+            } else {
+                while(left < right && nums[left] == nums[left + 1])
+                    left++;
+                left++;
+            }
+        }
+        
+        return false;
+    }
 }

d2045ce10ddf0bf3e2e3138d0a3ef3d1ee7284cd
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-26 15:06:03 -0400

    Update LC-Stock.java

--- Array/LC-Stock.java
+++ Array/LC-Stock.java
@@ -37,5 +37,43 @@ public class Solution {
         return max;
     }
     
+    /**
+     * LC123. Best Time to Buy and Sell Stock III
+     * 
+     * O(n^2) Time Limit Exceed
+     * 
+     */
+     public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len < 2)
+            return 0;
+            
+        int[] oneTran = new int[len];
+        int max = 0;
+        
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < len; i++) {
+            min = Math.min(min, prices[i]);
+            oneTran[i] = Math.max(0, prices[i] - min);
+            
+            int currMin = Integer.MAX_VALUE, currMax = 0;
+            for(int j = i + 1; j < len; j++) {
+                currMin = Math.min(currMin, prices[j]);
+                currMax = Math.max(currMax, prices[j] - currMin);
+            }
+            
+            max = Math.max(max, oneTran[i] + currMax);
+        }
+        
+        return max;
+    }
+    
+    /**
+     * LC123. Best Time to Buy and Sell Stock III
+     * 
+     * 
+     * 
+     */
+    
     
 }

dade31e12d93af66cfadb83dad32433fa89241a8
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-28 00:28:45 -0400

    Update LC-CombinationSum.java

--- Array/LC-CombinationSum.java
+++ Array/LC-CombinationSum.java
@@ -35,6 +35,9 @@ public class Solution {
         }
     }
     
+    
+    
+    
     /**
      * LC40. Combination SumII
      * 
@@ -70,4 +73,40 @@ public class Solution {
         }
     }
     
+    
+    
+    /**
+     * LC216. Combination Sum III;
+     * 
+     * only numbers from 1 to 9;
+     * k numbers add up to n;
+     * 
+     */
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new LinkedList<>();
+        List<Integer> tmp = new LinkedList<>();
+        
+        if (k > 9 || n < 0 || n > k * 9)
+            return res;
+        
+        prob(res, tmp, 9, n, k);
+        
+        return res;
+    }
+    
+    private void prob(List<List<Integer>> res, List<Integer> tmp, int start, int target, int num) {
+        if (target == 0 && num == 0) {
+            res.add(new LinkedList<>(tmp));
+            return;
+        }
+        
+        if (target < 0 || num <= 0) 
+            return;
+        
+        for(int i = start; i > 0; i--) {
+            tmp.add(i);
+            prob(res, tmp, i - 1, target - i, num - 1);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
 }

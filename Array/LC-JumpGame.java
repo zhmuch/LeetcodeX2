d0860da44009d27109a2cd2598d2d55d32ee4c18
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-28 22:17:27 -0400

    Update LC-JumpGame.java

--- Array/LC-JumpGame.java
+++ Array/LC-JumpGame.java
@@ -1,7 +1,7 @@
 public class Solution {
     
     /**
-     *LC55. Jump Game
+     * LC55. Jump Game
      * 
      */
     public boolean canJump(int[] nums) {
@@ -16,4 +16,31 @@ public class Solution {
         return false;
     }
     
+    
+    /**
+     * LC45. Jump Game II
+     * 
+     */
+    public int jump(int[] nums) {
+        if (nums.length <= 1)
+            return 0;
+        int start = 0, end = 0, max = 0, step = 0;
+        
+        while(true) {
+            for (int i = start; i <= end; i++) 
+                max = Math.max(max, i + nums[i]);
+            
+            if (max == end)
+                break;
+            start = end + 1;
+            end = max;
+            
+            step++;
+            
+            if (max >= nums.length - 1)
+                return step;
+        }
+        
+        return -1;
+    }
 }

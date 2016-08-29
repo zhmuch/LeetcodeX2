b0652c6bb82938374a849d943d0ea29c9d43129f
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-28 23:19:27 -0400

    Update LC-053.java

--- Array/LC-053.java
+++ Array/LC-053.java
@@ -14,4 +14,51 @@ public class Solution {
         
         return max;
     }
+    
+    /**
+     * Divide and Conquer;
+     * 
+     */
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
+
+        int pivot = (nums.length - 1) / 2;
+
+        return Math.max(maxSubArray(nums, 0, nums.length - 1, pivot), Math.max(maxSubArray(nums, 0, pivot), maxSubArray(nums, pivot + 1, nums.length - 1)));
+    }
+
+    private int maxSubArray(int[] nums, int left, int right) {
+        if (left == right)
+            return nums[left];
+
+        int pivot = (left + right) / 2;
+
+        return Math.max(maxSubArray(nums, left, right, pivot), Math.max(maxSubArray(nums, left, pivot), maxSubArray(nums, pivot + 1, right)));
+    }
+
+    private int maxSubArray(int[] nums, int left, int right, int pivot) {
+        if (left == right)
+            return nums[left];
+
+        int lmax = nums[pivot], tmp = nums[pivot];
+
+        for(int i = pivot - 1; i >= left; i--) {
+            tmp += nums[i];
+            lmax = Math.max(lmax, tmp);
+        }
+        
+        if (pivot >= right)
+            return lmax;
+
+        int rmax = nums[pivot + 1];
+        tmp = nums[pivot + 1];
+
+        for(int i = pivot + 2; i <= right; i++) {
+            tmp += nums[i];
+            rmax = Math.max(rmax, tmp);
+        }
+        
+        return lmax + rmax;
+    }
 }

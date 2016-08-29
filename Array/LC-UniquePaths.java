28651526d175318b3bf77fe4a5e8f1875acdda74
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-29 19:01:56 -0400

    Update LC-UniquePaths.java

--- Array/LC-UniquePaths.java
+++ Array/LC-UniquePaths.java
@@ -15,4 +15,33 @@ public class Solution {
         
         return (int)(up / down);
     }
+    
+    /**
+     * LC063. Unique Paths II
+     * 
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int row = obstacleGrid.length;
+        int col = obstacleGrid[0].length;
+        int[][] paths = new int[row][col];
+        
+        if (obstacleGrid[0][0] == 1)
+            return 0;
+        
+        paths[0][0] = 1;
+        for (int j = 1; j < col; j++)
+            if (obstacleGrid[0][j] == 1)
+                break;
+            else
+                paths[0][j] = 1;
+                
+        for (int i = 1; i < row; i++) {
+            paths[i][0] = (obstacleGrid[i][0] == 1) ? 0 : paths[i - 1][0];
+            
+            for (int j = 1; j < col; j++) 
+                paths[i][j] = (obstacleGrid[i][j] == 1) ? 0 : paths[i - 1][j] + paths[i][j - 1];
+        }        
+        
+        return paths[row - 1][col - 1];
+    }
 }

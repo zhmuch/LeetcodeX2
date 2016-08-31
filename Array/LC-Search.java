e4460f9cd14965940b949e5fdbe3cefdf6cc3f1e
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-31 16:01:57 -0400

    Update LC-Search.java

--- Array/LC-Search.java
+++ Array/LC-Search.java
@@ -36,4 +36,26 @@ public class Solution {
         
         return res;
     }
+    
+    
+    /**
+     * LC035. Search Insert Position
+     * 
+     */
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else 
+                right = mid - 1;
+        }
+        
+        return (target > nums[left]) ? left + 1 : left;
+    }
 }

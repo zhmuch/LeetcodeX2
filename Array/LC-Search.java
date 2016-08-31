99b6d4b3e2d54f1c3e123d1242f69904fc39d427
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-31 15:54:58 -0400

    Create LC-Search.java

---
+++ Array/LC-Search.java
@@ -0,0 +1,37 @@
+public class Solution {
+    /**
+     *
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = {-1, -1};
+        int idx = -1;
+        int left = 0, right = nums.length - 1;
+        
+        while(left <= right) {
+            int mid = (right - left) / 2 + left;
+            
+            if (nums[mid] == target) {
+                idx = mid;
+                break;
+            } else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        
+        if (idx == -1)
+            return res;
+        
+        right = idx;
+        while(right < nums.length && nums[right] == target)
+            right++;
+        
+        left = idx;
+        while(left >= 0 && nums[left] == target)
+            left--;
+            
+        res[0] = left + 1;
+        res[1] = right - 1;
+        
+        return res;
+    }
+}

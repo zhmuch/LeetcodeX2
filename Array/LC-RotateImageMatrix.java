cfb6a196b431da73a2effd6a72cda17968de1892
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-30 20:50:46 -0400

    Update and rename LC-048.java to LC-RotateImageMatrix.java

---
+++ Array/LC-RotateImageMatrix.java
@@ -0,0 +1,108 @@
+public class Solution {
+    /**
+     * LC048. Rotate Image
+     * 
+     */
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        
+        for (int p = 0; p < n / 2; p++) {
+            /* corner point (p, p) */
+            int edge = n - 2 * p;
+            
+            for (int offset = 0; offset < edge - 1; offset++) {
+                int tmp = matrix[p][p + offset];
+                
+                /* anticlockwise move */
+                matrix[p][p + offset] = matrix[n - p - 1 - offset][p];
+                matrix[n - p - 1 - offset][p] = matrix[n - p - 1][n - p - 1 - offset];
+                matrix[n - p - 1][n - p - 1 - offset] = matrix[p + offset][n - p - 1];
+                matrix[p + offset][n - p - 1] = tmp;
+            }
+        }
+        
+        return;
+    }
+    
+    /**
+     * LC054. Spiral Matrix
+     * 
+     * 
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new LinkedList<Integer>();
+
+        int row = matrix.length;
+        if (row < 1) return result;
+        int col = matrix[0].length;
+        if (col < 1) return result;
+
+        boolean[][] flag = new boolean[row][col];
+        for (int i=0; i<row; i++){
+            for (int j=0; j<col; j++){
+                flag[i][j] = true;
+            }
+        }
+
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        int direction = 0;
+
+        while (count < row * col){
+            switch (direction)
+            {
+                case 0: {
+                    while(j<col && flag[i][j]){
+                        result.add(matrix[i][j]);
+                        count++;
+                        flag[i][j]=false;
+                        j++;
+                    }
+                    j--;
+                    i++;
+                    direction = 1;
+                    break;
+                }
+                case 1: {
+                    while(i<row && flag[i][j]){
+                        result.add(matrix[i][j]);
+                        count++;
+                        flag[i][j]=false;
+                        i++;
+                    }
+                    i--;
+                    j--;
+                    direction = 2;
+                    break;
+                }
+                case 2: {
+                    while(j>=0 && flag[i][j]){
+                        result.add(matrix[i][j]);
+                        count++;
+                        flag[i][j]=false;
+                        j--;
+                    }
+                    j++;
+                    i--;
+                    direction = 3;
+                    break;
+                }
+                case 3: {
+                    while(i>=0 && flag[i][j]){
+                        result.add(matrix[i][j]);
+                        count++;
+                        flag[i][j]=false;
+                        i--;
+                    }
+                    i++;
+                    j++;
+                    direction = 0;
+                    break;
+                }
+            }
+
+        }
+        return result;
+    }
+}

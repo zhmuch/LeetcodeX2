f64c031fc93209099b4363d990fe777368947cb1
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-31 18:46:06 -0400

    Update LC-Rectangle.java

--- Array/LC-Rectangle.java
+++ Array/LC-Rectangle.java
@@ -127,3 +127,32 @@ public class Solution {
  * 85. Maximal Rectangle
  * 
  **********/
+
+/**
+ * 利用084
+ * 
+ */
+ public int maximalRectangle(char[][] matrix) {
+        int row = matrix.length;
+        if(row==0) return 0;
+        int col = matrix[0].length;
+
+        int[][] sta = new int[row][col];
+        for(int i=0; i<col; i++){
+            if (matrix[row-1][i] == '1') sta[row-1][i] = 1;
+            else sta[row-1][i] = 0;
+        }
+        for(int i=row-2; i>=0; i--){
+            for(int j=0; j<col; j++){
+                if (matrix[i][j] == '1') sta[i][j] = sta[i+1][j] + 1;
+                else sta[i][j] = 0;
+
+            }
+        }
+
+        int max = 0;
+        for(int i=0; i<row; i++){
+            max = Math.max(largestRectangleArea(sta[i]), max);
+        }
+        return max;
+    }

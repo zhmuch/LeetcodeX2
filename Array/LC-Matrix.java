d73367da27a76114f77ca0a796d40da0eb0782d4
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-31 17:26:20 -0400

    Update LC-Matrix.java

--- Array/LC-Matrix.java
+++ Array/LC-Matrix.java
@@ -24,4 +24,51 @@ public class Solution {
         
         return prev[col - 1];
     }
+    
+    
+    /**
+     * LC073. Set Matrix Zeroes
+     * 
+     */
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length, col = matrix[0].length;
+        boolean firstRow = false;
+        
+        //  Search for 0;
+        for (int j = 0; j < col; j++)
+            if (matrix[0][j] == 0) {
+                firstRow = true;
+                break;
+            }
+    
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    
+                    if (i != 0)
+                        matrix[i][0] = 0;
+                }
+        
+        //  Set 0;
+        for (int j = 1; j < col; j++)
+            if (matrix[0][j] == 0)
+                for (int i = 0; i < row; i++)
+                    matrix[i][j] = 0;
+        
+        for (int i = 1; i < row; i++)
+            if (matrix[i][0] == 0)
+                for (int j = 0; j < col; j++)
+                    matrix[i][j] = 0;
+        
+        if (matrix[0][0] == 0)
+            for (int i = 0; i < row; i++)
+                    matrix[i][0] = 0;
+        
+        if (firstRow)
+            for (int j = 0; j < col; j++)
+                    matrix[0][j] = 0;
+    
+        return;
+    }
 }

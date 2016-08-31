556ca32648fde735d78a12f2716aef752e9a311c
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-31 17:47:25 -0400

    Update LC-Matrix.java

--- Array/LC-Matrix.java
+++ Array/LC-Matrix.java
@@ -77,6 +77,32 @@ public class Solution {
      * LC074. Search a 2D Matrix
      * 
      */
+        public boolean searchMatrix(int[][] matrix, int target) {
+            if (matrix == null || matrix.length == 0) {
+                return false;
+            }
+            int start = 0, rows = matrix.length, cols = matrix[0].length;
+            int end = rows * cols - 1;
+            while (start <= end) {
+                int mid = (start + end) / 2;
+                if (matrix[mid / cols][mid % cols] == target) {
+                    return true;
+                } 
+                if (matrix[mid / cols][mid % cols] < target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+            return false;
+        }
+     
+     
+     
+    /**
+     * LC240. Search a 2D Matrix II
+     * 
+     */ 
     public boolean searchMatrix(int[][] matrix, int target) {
         int row = matrix.length, col = matrix[0].length;
         

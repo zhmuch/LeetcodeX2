d5287591e5ae05820eb7b83f401c5f3231c998b0
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-31 17:10:01 -0400

    Create LC-Matrix.java

---
+++ Array/LC-Matrix.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        int row = grid.length, col = grid[0].length;
+        int[] prev = new int[col], curr = new int[col];
+        
+        prev[0] = grid[0][0];
+        for (int j = 1; j < col; j++) 
+            prev[j] = prev[j - 1] + grid[0][j];
+            
+        for (int i = 1; i < row; i++) {
+            curr[0] = prev[0] + grid[i][0];
+            for (int j = 1; j < col; j++)
+                curr[j] = Math.min(curr[j - 1], prev[j]) + grid[i][j];
+                
+            int[] tmp = curr;
+            curr = prev;
+            prev = tmp;
+        }
+        
+        return prev[col - 1];
+    }
+}

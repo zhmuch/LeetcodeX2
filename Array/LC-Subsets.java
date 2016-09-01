931af9f5ad592416bac49db2e91df7a10f29c974
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-09-01 18:48:11 -0400

    Create LC-Subsets.java

---
+++ Array/LC-Subsets.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+        List<Integer> tmp = new LinkedList<>();
+        
+        search(res, tmp, nums, 0);
+        
+        return res;
+    }
+    
+    private void search(List<List<Integer>> res, List<Integer> tmp, int[] nums, int idx) {
+        if (idx >= nums.length) {
+            res.add(new LinkedList<>(tmp));
+            return;
+        }
+        
+        search(res, tmp, nums, idx + 1);
+        
+        tmp.add(nums[idx]);
+        search(res, tmp, nums, idx + 1);
+        tmp.remove(tmp.size() - 1);
+    }
+}

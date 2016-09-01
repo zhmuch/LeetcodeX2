9f9b8f5f62842537e9b6dd4272b0237cfde9127f
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-09-01 17:46:49 -0400

    Update LC-Permutation.java

--- Array/LC-Permutation.java
+++ Array/LC-Permutation.java
@@ -33,6 +33,7 @@ public class Solution {
     
     /**
      * LC046. Permutations
+     * ! ! ! ! ! ! 错误方法 ！ ！！！！！！！！！
      * 
      */
     public List<List<Integer>> permute(int[] nums) {
@@ -100,5 +101,49 @@ public class Solution {
         int tmp = nums[a];
         nums[a] = nums[b];
         nums[b] = tmp;
+    }
+    
+    /**
+     * LC046. Permutations
+     * 
+     * AC
+     * 
+     */
+    List<List<Integer>> res;
+    int[] nums;
+    boolean[] used;
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        res = new LinkedList<>();
+        if(nums == null || nums.length < 1)
+            return res;
+
+        this.nums = nums;
+        Arrays.sort(this.nums);
+        used = new boolean[nums.length];
+
+        generate(new LinkedList<>(), 0);
+
+        return res;
+    }
+
+    private void generate(List<Integer> curr, int x){
+        if(x == nums.length)
+            res.add(new LinkedList<>(curr));
+        else {
+            for (int i = 0; i < nums.length; i++)
+                if (!used[i]) {
+                    used[i] = true;
+                    curr.add(nums[i]);
+                    
+                    generate(curr, x + 1);
+
+                    used[i] = false;
+                    curr.remove(curr.size() - 1);
+
+                    while (i < nums.length - 1 && nums[i] == nums[i + 1])
+                        i++;
+                }
+        }
     }
 }

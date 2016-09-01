24f52fe510b559737981379a59cea84478e91f4c
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-09-01 18:39:24 -0400

    Update LC-Permutation.java

--- Array/LC-Permutation.java
+++ Array/LC-Permutation.java
@@ -148,4 +148,41 @@ public class Solution {
                 }
         }
     }
+    
+    
+    /**
+     * LC060. Permutation Sequence
+     * 
+     * 
+     */
+    public String getPermutation(int n, int k) {
+        if(n == 1)
+            return "1";
+
+        String r = "";
+
+        LinkedList<Integer> ref = new LinkedList<>();
+
+        int s = 1;
+        for(int i=1; i<=n; i++) {
+            ref.add(i);
+            s *= i;
+        }
+
+        for(int i=n; i>0; i--){
+            s /= i;
+            if(k != 0 && k % s == 0) {
+                r = r + ref.remove(k / s - 1);
+                break;
+            }
+            r = r + ref.remove(k / s);
+            k %= s;
+        }
+
+        
+        while(!ref.isEmpty())
+            r = r + ref.removeLast();
+      
+        return r;
+    }
 }

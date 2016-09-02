596a3aeaf34b1e3420d230c43498a5f8b46f7fca
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-09-02 02:14:02 -0400

    Update LC-Duplicates.java

--- Array/LC-Duplicates.java
+++ Array/LC-Duplicates.java
@@ -1,6 +1,38 @@
 public class Solution {
-
     
+    /**
+     * 用Bit位来代替Set，
+     * 储存出现过的数字；
+     */
+    public boolean containsDuplicate(int[] nums) {
+        byte[] mark = new byte[150000];
+        for (int i : nums) {
+            int j = i/8;
+            int k = i%8;
+            int check = 1<<k;
+            if ((mark[j] & check) != 0) {
+                return true;
+            }
+            mark[j]|=check;
+        }
+        return false;
+    }
+    
+    
+    /**
+     * 排序
+     * 
+     * 再逐一判重；
+     * 
+     * 低效
+     * 
+     */
+    
+
+    /**
+     * HashSet
+     * 超时！
+     */
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         for (int i : nums)

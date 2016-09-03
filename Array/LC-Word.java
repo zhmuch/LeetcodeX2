029bab21178203958632110b483b2939bbb9db37
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-09-03 14:21:21 -0400

    Update LC-Word.java

--- Array/LC-Word.java
+++ Array/LC-Word.java
@@ -2,6 +2,8 @@ public class Solution {
   /**
    * LC079. Word Search
    * 
+   * O(n) space
+   * 
    */
     public boolean exist(char[][] board, String word) {
         int row = board.length;
@@ -49,5 +51,36 @@ public class Solution {
         }
         
         return false;
+    }
+    
+    
+    
+    /**
+     * LC079. Word Search
+     * 
+     * O(1) space
+     * 
+     */
+    public boolean exist(char[][] board, String word) {
+        char[] w = word.toCharArray();
+        for (int y=0; y<board.length; y++) {
+        	for (int x=0; x<board[y].length; x++) {
+        		if (exist(board, y, x, w, 0)) return true;
+        	}
+        }
+        return false;
+    }
+    
+    private boolean exist(char[][] board, int y, int x, char[] word, int i) {
+    	if (i == word.length) return true;
+    	if (y<0 || x<0 || y == board.length || x == board[y].length) return false;
+    	if (board[y][x] != word[i]) return false;
+    	board[y][x] ^= 256;
+    	boolean exist = exist(board, y, x+1, word, i+1)
+    		|| exist(board, y, x-1, word, i+1)
+    		|| exist(board, y+1, x, word, i+1)
+    		|| exist(board, y-1, x, word, i+1);
+    	board[y][x] ^= 256;
+    	return exist;
     }
 }

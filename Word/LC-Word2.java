4e0df22d45347ff6e8fb3089449179beb4fc291c
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-09-11 15:48:28 -0400

    Update LC-Word2.java

--- Word/LC-Word2.java
+++ Word/LC-Word2.java
@@ -21,4 +21,39 @@ public class Solution {
         
         return (len == 0) ? Math.max(prev, len) : len;
     }
+    
+    
+    /**
+     * LC-151. Reverse Words in a String
+     * 
+     * 
+     */
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        int len = s.length();
+
+        int idx = 0, start = -1, end = -1;
+        while (idx < len) {
+            while (idx < len && s.charAt(idx) == ' ')
+                idx++;
+
+            start = idx;
+            while (idx < len && s.charAt(idx) != ' ')
+                idx++;
+            end = idx - 1;
+
+            for (; start <= end; end--) {
+                sb.insert(0, s.charAt(end));
+            }
+
+            if (idx == len)
+                break;
+            else
+                sb.insert(0, ' ');
+        }
+
+        if (sb.length() > 0 && sb.charAt(0) == ' ')
+            sb.deleteCharAt(0);
+        return sb.toString();
+    }
 }

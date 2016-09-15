5e6cc59f9484092d6d8d4b0787485883904b8293
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-09-15 19:08:57 -0400

    Update LC-128-Longest Consecutive Sequence.java

--- Array/LC-128-Longest Consecutive Sequence.java
+++ Array/LC-128-Longest Consecutive Sequence.java
@@ -20,4 +20,34 @@ public class Solution {
         
         return max;
     }
+    
+    /**
+     * Union Find
+     * 
+     * O(n)
+     * 
+     */
+    public int longestConsecutive(int[] nums) {
+        int max = 1;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            if (!map.containsKey(i)) {
+                int left = (map.containsKey(i - 1)) ? map.get(i - 1) : 0;
+                int right = (map.containsKey(i + 1)) ? map.get(i + 1) : 0;
+                
+                int sum = left + right + 1;
+                
+                max = Math.max(max, sum);
+                
+                map.put(i, sum);
+                
+                // Update Boundary;
+                map.put(i - left, sum);
+                map.put(i + right, sum);
+            }
+        }
+        
+        return max;
+    }
 }

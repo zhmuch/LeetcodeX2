a84c0c5a8f74cace7c283c81e2e985084af81fae
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-09-15 17:03:39 -0400

    Create LC128-Longest Consecutive Sequence.java

---
+++ Array/LC128-Longest Consecutive Sequence.java
@@ -0,0 +1,19 @@
+public class Solution {
+  /**
+    public int longestConsecutive(int[] nums) {
+        Arrays.sort(nums);
+        
+        int count = 1, max = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] > 1)
+                count = 1;
+            
+            if (nums[i] - nums[i - 1] == 1)
+                count++;
+
+            max = Math.max(count, max);
+        }
+        
+        return max;
+    }
+}

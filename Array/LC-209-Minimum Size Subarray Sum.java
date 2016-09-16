2a25fd110f215fd64e90d1507c430a84d142c576
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-09-15 20:21:15 -0400

    Create LC-209-Minimum Size Subarray Sum.java

---
+++ Array/LC-209-Minimum Size Subarray Sum.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        if (nums == null || nums.length < 1)
+            return 0;
+        int left = 0, right = 0, sum = nums[0], len = Integer.MAX_VALUE;
+        
+        while (true) {
+            if (sum < s) {
+                right++;
+                if (right >= nums.length)
+                    break;
+                sum += nums[right];
+            } else {
+                len = Math.min(len, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        
+        return (len == Integer.MAX_VALUE) ? 0 : len;
+    }
+}

d09dab5d669bd4e9fb0a7795ca1662477ded9dff
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-09-16 12:08:06 -0400

    Update LC-396.java

--- Array/LC-396.java
+++ Array/LC-396.java
@@ -1,6 +1,34 @@
 public class Solution {
     /**
+     * O(n)
+     * 
+     */
+    public int maxRotateFunction(int[] nums) {
+        if (nums == null || nums.length < 1)
+            return 0;
+        
+        int curr = 0, sum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            curr += i * nums[i];
+        }
+        
+        int max = curr;
+        
+        for (int i = 1; i < nums.length; i++) {
+            curr = curr + sum - nums[nums.length - i] * nums.length;
+            
+            max = Math.max(max, curr);
+        }
+        
+        return max;
+    }
+    
+    /**
      * Time Limit Exceed;
+     * 
+     * O(n^2)
      * 
      */
     public int maxRotateFunction(int[] A) {

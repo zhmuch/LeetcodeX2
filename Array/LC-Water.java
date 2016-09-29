47746fb50e720478d1c92b2622fac0161ecef25f
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-09-28 22:13:27 -0400

    Update LC-Water.java

--- Array/LC-Water.java
+++ Array/LC-Water.java
@@ -105,4 +105,80 @@ public class Solution {
         
         return vol;
     }
+    
+    
+    /**
+     * LC-407. Trapping Rain Water II
+     *
+     */
+    import java.io.*;
+    import java.util.*;
+
+    public class Solution {
+        private class Cell {
+            int row;
+            int col;
+            int height;
+            public Cell(int row, int col, int height) {
+                this.row = row;
+                this.col = col;
+                this.height = height;
+            }
+        }
+
+        public int trapRainWater(int[][] heightMap) {
+            if (heightMap == null || heightMap.length == 0 || heightMap[0].length == 0)
+                return 0;
+
+            PriorityQueue<Cell> queue = new PriorityQueue<>(new Comparator<Cell>() {
+                @Override
+                public int compare(Cell o1, Cell o2) {
+                    return o1.height - o2.height;
+                }
+            });
+
+
+            int row = heightMap.length;
+            int col = heightMap[0].length;
+
+            boolean[][] visited = new boolean[row][col];
+
+            // Initial Boundary
+            for (int i = 0; i < row; i++) {
+                visited[i][0] = visited[i][col - 1] = true;
+                queue.offer(new Cell(i, 0, heightMap[i][0]));
+                queue.offer(new Cell(i, col - 1, heightMap[i][col - 1]));
+            }
+            for (int j = 0; j < col; j++) {
+                visited[0][j] = visited[row - 1][j] = true;
+                queue.offer(new Cell(0, j, heightMap[0][j]));
+                queue.offer(new Cell(row - 1, j, heightMap[row - 1][j]));
+            }
+
+            int res = 0;
+            int[][] dire = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+            while (!queue.isEmpty()) {
+                Cell curr = queue.poll();
+
+                for (int[] d : dire) {
+                    int tr = curr.row + d[0];
+                    int tc = curr.col + d[1];
+
+                    if (0 <= tr && tr < row && 0 <= tc && tc < col && !visited[tr][tc]) {
+                        visited[tr][tc] = true;
+
+                        if (heightMap[tr][tc] < curr.height) {
+                            res += (curr.height - heightMap[tr][tc]);
+                            queue.offer(new Cell(tr, tc, curr.height));
+                        } else {
+                            queue.offer(new Cell(tr, tc, heightMap[tr][tc]));
+                        }
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
 }

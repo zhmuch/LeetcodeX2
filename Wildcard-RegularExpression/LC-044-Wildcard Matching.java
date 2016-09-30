857e22edf2dd84bac390b44ae0fff4d1bfa6cce2
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-09-30 11:06:03 -0400

    Update LC-044-Wildcard Matching.java

--- Wildcard-RegularExpression/LC-044-Wildcard Matching.java
+++ Wildcard-RegularExpression/LC-044-Wildcard Matching.java
@@ -1,4 +1,30 @@
 public class Solution {
+    /**
+     * Greedy + Search
+     * 用两个指针save_i, save_j代替递归
+     */
+    public boolean isMatch(String s, String p) {
+        char p_ar[] = p.toCharArray();
+        char s_ar[] = s.toCharArray();
+        int save_i, save_j, i, j;
+        save_i = save_j = -1;
+        i = j = 0;
+        while(i < s_ar.length){
+            if(j < p_ar.length && (s_ar[i] == p_ar[j] || p_ar[j] == '?')){
+                ++i;
+                ++j;
+            }else if(j < p_ar.length && p_ar[j] == '*'){
+                save_i = i + 1;
+                save_j = j;
+                ++j;
+            }else if(save_i != -1){
+                i = save_i;
+                j = save_j;
+            }else return false;
+        }
+        while(j < p_ar.length)if(p_ar[j++] != '*')return false;
+        return i == s_ar.length && j == p_ar.length;
+    }
     
     /**
      * DP

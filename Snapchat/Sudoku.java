6bfe753c92aa9e8a0319cc53e7f68e86e3e589e3
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-10-16 18:32:12 -0400

    Update Sudoku.java

--- Snapchat/Sudoku.java
+++ Snapchat/Sudoku.java
@@ -69,4 +69,68 @@ public class Solution {
         }
         return true;
     }
+    
+    /** 
+     *  37. Sudoku Solver
+     */
+    public class Solution {
+        public void solveSudoku(char[][] board) {
+            // There are 27 sets
+            // 0..8:    row
+            // 9..17:   col
+            // 18..26:  block
+
+            Hashtable<Integer, Set<Integer>> table = new Hashtable<>();
+            for (int i = 0; i < 27; i++)
+                table.put(i, new HashSet<Integer>());
+
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                    if (board[i][j] == '.')
+                        continue;
+                    else {
+                        int t = board[i][j] - '0';
+                        table.get(i).add(t);
+                        table.get(9 + j).add(t);
+                        table.get((i / 3) * 3 + (j / 3) + 18).add(t);
+                    }
+
+            solver(board, table, 0, 0);
+        }
+
+        private boolean solver(char[][] board, Hashtable<Integer, Set<Integer>> table, int r, int c) {
+
+            if (r >= 9)
+                return true;
+
+            if (c >= 9)
+                return solver(board, table, r + 1, 0);
+
+            if (board[r][c] != '.')
+                return solver(board, table, r, c + 1);
+
+            for (int i = 1; i <= 9; i++) {
+
+                if (!table.get(r).contains(i) && !table.get(9 + c).contains(i) &&
+                        !table.get((r / 3) * 3 + (c / 3) + 18).contains(i)) {
+                    
+                    table.get(r).add(i);
+                    table.get(9 + c).add(i);
+                    table.get((r / 3) * 3 + (c / 3) + 18).add(i);
+                    board[r][c] = (char)('0' + i);
+
+                    if (solver(board, table, r, c + 1))
+                        return true;
+
+                    table.get(r).remove(i);
+                    table.get(9 + c).remove(i);
+                    table.get((r / 3) * 3 + (c / 3) + 18).remove(i);
+                    board[r][c] = '.';
+                }
+            }
+
+            return false;
+        }
+
+    }
 }

cb00088e61bf68090b72a1f997c64d6dcf971275
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-10-16 15:07:37 -0400

    Create Sudoku.java

---
+++ Snapchat/Sudoku.java
@@ -0,0 +1,51 @@
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        if (board == null || board.length != 9 || board[0].length != 9)
+            return false;
+        
+        for (int i = 0; i < 9; i++)
+            if (!isValid(board, i, 0, i, 8) || !isValid(board, 0, i, 8, i) || !isValid(board, (i / 3) * 3, (i % 3) * 3))
+                return false;
+        
+        return true;
+    }
+    
+    private boolean isValid(char[][] board, int r, int c) {
+        boolean[] used = new boolean[9];
+        
+        for (int i = r; i < r + 3; i++)
+            for (int j = c; j < c + 3; j++)
+                if (board[i][j] == '.')
+                    continue;
+                else if (used[board[i][j] - '1'])
+                    return false;
+                else
+                    used[board[i][j] - '1'] = true;
+        
+        return true;
+    }
+    
+    private boolean isValid(char[][] board, int r1, int c1, int r2, int c2) {
+        boolean[] used = new boolean[9];
+        
+        if (r1 == r2) {
+            for (int i = c1; i <= c2; i++)
+                if (board[r1][i] == '.')
+                    continue;
+                else if (used[board[r1][i] - '1'])
+                    return false;
+                else
+                    used[board[r1][i] - '1'] = true;
+        } else {
+            for (int i = r1; i <= r2; i++)
+                if (board[i][c1] == '.')
+                    continue;
+                else if (used[board[i][c1] - '1'])
+                    return false;
+                else
+                    used[board[i][c1] - '1'] = true;
+        }
+        
+        return true;
+    }
+}

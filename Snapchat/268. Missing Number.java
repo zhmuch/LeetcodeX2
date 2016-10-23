f41c6262b0ce00035ee024070f1a2b8b189b9599
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-10-23 01:59:12 -0400

    Update 268. Missing Number.java

--- Snapchat/268. Missing Number.java
+++ Snapchat/268. Missing Number.java
@@ -1,4 +1,7 @@
 public class Solution {
+    /**
+     * 方法一：
+     */
     public int missingNumber(int[] nums) {
         int max = nums.length;
         
@@ -19,5 +22,22 @@ public class Solution {
         int tmp = nums[idx1];
         nums[idx1] = nums[idx2];
         nums[idx2] = tmp;
+    }
+    
+    
+    /**
+     * 方法二
+     */
+    public int missingNumber(int[] nums) {
+        int allXOR = 0, actXOR = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            allXOR ^= i;
+            actXOR ^= nums[i];
+        }
+        
+        allXOR ^= nums.length;
+        
+        return allXOR ^ actXOR;
     }
 }
